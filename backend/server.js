5b9d882e74d9abafa821bc2a8674c9e0cb6756e9
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-12 20:31:43 +0300

    style: use uppercase PORT constant for readability

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to GoalApp API");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${port}`);
 });
