399fbda5ae60fa88bb859e8c38531843c56af0f5
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-13 12:08:35 +0300

    feat: install dotenv and move PORT to .env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Welcome to GoalApp API");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
