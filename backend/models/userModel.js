dcc8482df1b8f12b659ee461547de3004e04bde9
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-17 16:09:37 +0300

    feat: create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please insert a name"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please insert a password"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please insert an email"],
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
