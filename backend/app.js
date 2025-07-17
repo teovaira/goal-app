d467e417bfcc0704704fb5afa822d23c9d7e4d32
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-17 15:58:27 +0300

    feat: add notFoundRouteMiddleware for handling with flexibility invalid routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
 const errorHandler = require("./middlewares/errorMiddleware");
+const notFound = require("./middlewares/notFoundRouteMiddleware");
 const goalRoutes = require("./routes/goalRoutes");
 
 app.use(express.json());
 
-app.use("/api/goals", goalRoutes);
-
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
   res.send("Welcome to GoalApp API");
 });
+
+app.use("/api/goals", goalRoutes);
+
+// app.use((req, res, next) => {
+//   res.status(404).json({message: "Route not found."});
+// });
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 module.exports = app;
 
