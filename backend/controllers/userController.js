eb4b438c0277e14a0a8df32955de84d816801ef5
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-17 16:42:57 +0300

    feat: create registerUser controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+
+  const { name, password, email} = req.body;
+
+  const userExists = await User.findOne({email});
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
+
+  const SaltOrRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, SaltOrRounds);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data")
+  };
+
+};
+
+module.exports = registerUser;
