6efffc20e0c535c7a777e6378cc33fbd187b3601
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-17 13:52:55 +0300

    refactor: uddate and delete controllers with asyncHandler and throw new error pattern

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -35,7 +35,7 @@ const createGoal = asyncHandler(async (req,res) => {
 
   if (!req.body || !req.body.text) {
     res.status(400);
-    throw new error("Please add a text field.");
+    throw new Error("Please add a text field.");
   }
 
   const goal = await Goal.create({
@@ -50,19 +50,22 @@ const createGoal = asyncHandler(async (req,res) => {
   const updateGoal = asyncHandler(async (req, res) => {
     
       if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({ error: "Invalid goal id."});
+        res.status(400);
+        throw new Error("Invalid goal id.");
       }
       const goal = await Goal.findById(req.params.id);
 
       if(!goal) {
         console.log("Goal not found.");
-        return res.status(404).json({"error": "Goal not found."});
+        res.status(404);
+        throw new Error("Goal not found.");
       };
 
-      if (!req.body || !req.body.text) {
+      if (!req.body?.text) {
         console.log("Missing request body or text field.");
-        return res.status(400).json({error: "Please include a valid text field."});
-      }
+        res.status(400);
+        throw new Error("Please add a text field.")
+      };
 
       const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
 
@@ -72,31 +75,27 @@ const createGoal = asyncHandler(async (req,res) => {
     
   });
 
-  const deleteGoal = async (req,res) => {
+  const deleteGoal = asyncHandler( async (req,res) => {
     
-    try {
       if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return res.status(400).json({ error: "Invalid goal id." });
-      }
+        res.status(400);
+        throw new Error("Invalid goal id.");
+      };
+
       const goal = await Goal.findById(req.params.id);
       if (!goal) {
-        return res.status(404).json({error: "Goal not found"});
+        res.status(404);
+        throw new Error("Goal not found.");
       };
 
       const deletedGoal = await goal.deleteOne();
 
-      res
-        .status(200)
-        .json({
-          message: `Goal with id ${req.params.id} deleted successfully`});
+      res.status(200).json(
+        "Goal successfully deleted."
+      );
 
-    } catch (error) {
-
-      console.error("Error in deleting goal: ", error.message);
-      return res.status(500).json({ error: "server error" });
-
-    };
-  }  
+    
+  });
 
 
 module.exports = {
