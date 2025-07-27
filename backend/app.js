4edeb9e5c769788e8da7ba8ac431dfe322b1a4e1
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-27 23:39:32 +0300

    feat(logging): setup morgan in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const passport = require("passport");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const logger = require("./config/logger");
 require("./config/passport");
 const express = require("express");
 const app = express();
@@ -12,10 +16,34 @@ const goalRoutes = require("./routes/goalRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 
+
+
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+const logDirectory = path.join(__dirname, "logs");
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+const accessLogStream = fs.createWriteStream(
+  path.join(logDirectory, "access.log"),
+  { flags: "a" }
+);
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use(
+  morgan("dev", {
+    skip: (req, res) => res.statusCode < 400,
+    stream: {
+      write: (message) => logger.error(message.trim()),
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to GoalApp API");
