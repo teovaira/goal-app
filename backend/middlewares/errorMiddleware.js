c56359fb979714e885c660ce50dbb36eb9da789d
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-28 14:40:59 +0300

    chore: add winston loggers to errorMiddleware

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,11 +1,18 @@
-const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+const logger = require("../config/logger");
 
-  console.log("Error message: ", err.message);
+const errorHandler = (err, req, res, next) => {
+  const statusCode =
+    res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+
+  
+  logger.error(`[${req.method}] ${req.originalUrl} - ${err.message}`);
+  if (process.env.NODE_ENV !== "production") {
+    logger.debug(`Stack Trace: ${err.stack}`);
+  }
 
   res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
