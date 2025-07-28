38cbbde473d11c680702ec48387650ca5f167f1a
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-07-28 15:10:54 +0300

    chore: add winston loggers to notFoundRouteMiddleware

--- backend/middlewares/notFoundRouteMiddleware.js
+++ backend/middlewares/notFoundRouteMiddleware.js
@@ -1,5 +1,9 @@
+const logger = require("../config/logger");
+
 const notFound = (req, res, next) => {
-  const error = new Error(`Route - ${req.originalUrl} - not found`);
+  const errorMessage = `Route: ${req.originalUrl} not found `;
+  logger.warn(errorMessage);
+  const error = new Error(errorMessage);
   res.status(404);
   next(error);
 };
