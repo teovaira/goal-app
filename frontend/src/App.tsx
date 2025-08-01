3b1efd1adccfc3c5ae558d9b6f01fb3b06b861f5
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-01 21:27:05 +0300

    feat: add routing for the Dashboard component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
