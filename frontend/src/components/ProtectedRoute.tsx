95a18adcf367d6fa27c4994f1fe0f7df0407b5c1
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-02 18:36:26 +0300

    feat: create ProtectedRoute component to deny access to users that can not login, to the dashboard

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.tsx"; 
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isAuthenticated } = useAuth(); 
+
+  
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
