b09d4af60bef7a73bb54977ab5104a9e66b0dbea
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-03 22:19:21 +0300

    feat: add isLoading and showSpinner state and a timing function for smoother transition for user experience

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import useAuth from "../context/useAuth"; 
 
@@ -7,8 +7,29 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated } = useAuth(); 
+  const { isAuthenticated, isLoading } = useAuth(); 
+  const [showSpinner, setShowSpinner] = useState<boolean>(false);
 
+  useEffect(() => {
+    if (isLoading) {
+      const timer = setTimeout(() => {
+        setShowSpinner(true);
+      }, 500); 
+
+      return () => clearTimeout(timer); 
+    } else {
+      setShowSpinner(false); 
+    }
+  }, [isLoading]);
+
+  
+  if (showSpinner) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="spinner"></div> 
+      </div>
+    );
+  }
   
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
