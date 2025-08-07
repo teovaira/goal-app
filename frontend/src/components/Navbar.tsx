d0eb07de9571f5038fc364ab5e1d1c14580c5892
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-07 16:44:57 +0300

    feat: add auth state to navbar and conditional rendering to show the appropriate links depending on user being logged in or not

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import useAuth from "../context/useAuth";
 
 const Navbar = () => {
+  const { user, isAuthenticated } = useAuth();
+  
   return (
     <nav className="bg-gray-800 p-4">
       <ul className="flex space-x-4">
@@ -10,16 +13,26 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        <li>
-          <Link to="/login" className="text-white">
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register" className="text-white">
-            Register
-          </Link>
-        </li>
+        {isAuthenticated ? (
+          <li>
+            <Link to="/dashboard" className="text-white">
+              Dashboard
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login" className="text-white">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="text-white">
+                Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
