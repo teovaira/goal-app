3f66dfd02f990ddbe0f97c887990247d1bd7cb6b
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-09 20:36:16 +0300

    feat: update the jest configurarion

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,30 @@
 module.exports = {
   testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  collectCoverageFrom: [
+    "controllers/**/*.js",
+    "models/**/*.js",
+    "middlewares/**/*.js",
+    "utils/**/*.js",
+    "routes/**/*.js",
+    "!**/node_modules/**",
+    "!**/tests/**"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  testMatch: [
+    "**/tests/**/*.test.js",
+    "**/tests/**/*.spec.js"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
 };
