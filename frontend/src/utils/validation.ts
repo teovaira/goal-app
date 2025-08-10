cc22584e6b5c76c61c4ce3bcfec7f8b79cb8a7d0
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-10 13:45:41 +0300

    refactor: move regex validation to helper function

---
+++ frontend/src/utils/validation.ts
@@ -0,0 +1,10 @@
+export const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const passwordRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const validateEmail = (email: string): boolean => {
+  return emailRegex.test(email);
+};
+
+export const validatePassword = (password: string): boolean => {
+  return passwordRegex.test(password);
+};
