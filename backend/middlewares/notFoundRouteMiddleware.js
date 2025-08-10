f3d91676c5f082778c01fb6afe8f6d28a2a9187d
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-10 13:58:11 +0300

    fix: correct middleware content

--- backend/middlewares/notFoundRouteMiddleware.js
+++ backend/middlewares/notFoundRouteMiddleware.js
@@ -1,13 +1,11 @@
-const jwt = require("jsonwebtoken");
 const logger = require("../config/logger");
 
-const generateToken = (userId) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "2h",
-  });
-
-  logger.debug(`Generated token for user ${userId}`);
-  return token;
+const notFound = (req, res, next) => {
+  logger.warn(`404 - Not Found: ${req.originalUrl}`);
+  
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 
-module.exports = generateToken;
+module.exports = notFound;
