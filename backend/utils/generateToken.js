996a29b7c7bf33f1361a94061863fefcef4c8b63
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-10 13:00:56 +0300

    fix: remove security issue by removing jwt secret console.log

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  console.log("Generating token with secret:", process.env.JWT_SECRET);
   return jwt.sign({id}, process.env.JWT_SECRET, {
     expiresIn: "2h"
   });
