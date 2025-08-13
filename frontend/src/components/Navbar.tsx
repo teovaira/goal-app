27ba64521b8aea254d9f59fcb14b050867ddc42d
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-13 16:28:31 +0300

    refactor: update the name of the app in Goal Tracker

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="text-white font-bold text-xl">
-              Goal App
+              Goal Tracker
             </Link>
             <div className="hidden md:block ml-10">
               <div className="flex items-baseline space-x-4">
