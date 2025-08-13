de3bc5044790cb9558f799455ce090f53cce9db0
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-13 16:31:04 +0300

    refactor: change the name of the app in Goal Tracker

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <main className="flex justify-center items-center min-h-screen bg-gray-100 px-4 py-8">
       <div className="text-center p-6 sm:p-8 bg-white rounded-lg shadow-lg max-w-lg w-full">
         <h1 className="text-2xl sm:text-3xl font-bold">Home Page</h1>
-        <p className="mt-4 text-base sm:text-lg">Welcome to the Goal App!</p>
+        <p className="mt-4 text-base sm:text-lg">Welcome to the Goal Tracker!</p>
       </div>
     </main>
   );
