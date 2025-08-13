f881e90edbd0dcea744cfa3dd8aa2009c8f25610
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-08-13 16:20:50 +0300

    refactor: update the name of the app into Goal Tracker

--- backend/docs/swagger.js
+++ backend/docs/swagger.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "GoalApp API",
+      title: "Goal Tracker API",
       version: "1.0.0",
       description: "API for managing user goals and authentication",
     },
